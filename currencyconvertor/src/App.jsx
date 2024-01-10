089410f550fd86ae2e29f75261bd9ff0670a8a63
2003devansh <100955479+2003devansh@users.noreply.github.com>
2024-01-10 11:11:07 +0530

    first commit

--- currencyconvertor/src/App.jsx
+++ currencyconvertor/src/App.jsx
@@ -1,7 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react' 
 import './App.css'
+import Input from './components/input'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +8,7 @@ function App() {
   return (
     <>
       <h1 className='text'>hi i am devnash</h1>
+      <Input/>
     </>
   )
 }
